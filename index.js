0924434caa318ea67a1175d13e6c1743eba3145f
Posandu Mapa <posandumapa@gmail.com>
2023-02-15 13:04:13 +0530

    g

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-import { Client, GatewayIntentBits, EmbedBuilder, Partials, AttachmentBuilder, time, ActionRowBuilder, ButtonBuilder, ButtonStyle, REST, Routes } from "discord.js";
+import {
+	Client,
+	GatewayIntentBits,
+	EmbedBuilder,
+	Partials,
+	time,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	REST,
+	Routes,
+} from "discord.js";
 import express from "express";
-import fetch from 'node-fetch';
-import { createCanvas, loadImage } from "@napi-rs/canvas";
-import { request } from "undici";
+import fetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT || 80;
@@ -15,7 +24,6 @@ app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
-
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -25,7 +33,7 @@ const client = new Client({
 		GatewayIntentBits.GuildPresences,
 		GatewayIntentBits.DirectMessages,
 		GatewayIntentBits.DirectMessageTyping,
-		GatewayIntentBits.GuildMessageReactions
+		GatewayIntentBits.GuildMessageReactions,
 	],
 	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
@@ -36,29 +44,32 @@ client.once("ready", async () => {
 	let i = 0;
 
 	client.user.setPresence({
-		activities: [{
-			name: "Just woke up"
-		}],
-		status: "online"
-	})
+		activities: [
+			{
+				name: "Just woke up",
+			},
+		],
+		status: "online",
+	});
 
 	setInterval(() => {
-		let stuff = "with your mom,with daddy,with code,with Twitter,with bed".split(",")
+		let stuff =
+			"with your mom,with daddy,with code,with Twitter,with bed".split(",");
 
 		client.user.setPresence({
-			activities: [{ name: stuff[i % 5] }]
+			activities: [{ name: stuff[i % 5] }],
 		});
 
 		i++;
-	}, 40000)
+	}, 40000);
 
 	const debugChannel = client.channels.cache.get("1013035884481892382");
 
-	const startMsg = await debugChannel.send("Bot started on " + time(new Date(), "R"))
+	const startMsg = await debugChannel.send(
+		"Bot started on " + time(new Date(), "R")
+	);
 
-
-
-	startMsg.react("🎉")
+	startMsg.react("🎉");
 
 	app.get("/a", async (req, res) => {
 		debugChannel.send("opened /a");
@@ -68,19 +79,21 @@ client.once("ready", async () => {
 client.on("messageCreate", async (message) => {
 	if (message.author.bot) return;
 	if (message.content.startsWith("___dev")) {
-		if (message.author.id !== "961161387101536296") return message.channel.send(message.author.toString() + " You have no perms to run this!!!");
+		if (message.author.id !== "961161387101536296")
+			return message.channel.send(
+				message.author.toString() + " You have no perms to run this!!!"
+			);
 
 		const id = (Math.random() + Date()).replace(/[^a-zA-z0-9]/, "");
 
-		const row = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('primary')
-					.setLabel('Click me!')
-					.setStyle(ButtonStyle.Primary),
-			);
+		const row = new ActionRowBuilder().addComponents(
+			new ButtonBuilder()
+				.setCustomId("primary")
+				.setLabel("Click me!")
+				.setStyle(ButtonStyle.Primary)
+		);
 
-		await message.reply({ content: 'I think you should,', components: [row] });
+		await message.reply({ content: "I think you should,", components: [row] });
 
 		return;
 	}
@@ -91,7 +104,9 @@ client.on("messageCreate", async (message) => {
 		message.react("🆗");
 	}
 	if (message.content.startsWith("secretSay")) {
-		const channel = message.channel.send(message.content.replace("secretSay", ""));
+		const channel = message.channel.send(
+			message.content.replace("secretSay", "")
+		);
 		message.delete();
 	}
 	//
@@ -102,13 +117,17 @@ client.on("messageCreate", async (message) => {
 
 	let commands = {
 		ping: async () => {
-			const msg = await message.channel.send(`Pong! Latency is ${client.ws.ping}ms.`);
+			const msg = await message.channel.send(
+				`Pong! Latency is ${client.ws.ping}ms.`
+			);
 			let i = 0;
 			message.react("🕺");
 			const intrvl = setInterval(() => {
 				msg.edit(`Pong! Latency is ${client.ws.ping}ms. [${++i}]`);
 			}, 1000);
-			setTimeout(() => { clearInterval(intrvl) }, 6000);
+			setTimeout(() => {
+				clearInterval(intrvl);
+			}, 6000);
 		},
 		help: () => {
 			const c = Object.keys(commands);
@@ -130,7 +149,10 @@ client.on("messageCreate", async (message) => {
 		say: () => {
 			message.channel.send({
 				content: [...args].join(" ") || "no message :(",
-				allowedMentions: { users: ['123456789012345678'], roles: ['102938475665748392'] },
+				allowedMentions: {
+					users: ["123456789012345678"],
+					roles: ["102938475665748392"],
+				},
 			});
 		},
 		match: async () => {
@@ -171,9 +193,7 @@ client.on("messageCreate", async (message) => {
 			);
 			const rand = () => users.random();
 
-			const msg = await message.channel.send(
-				`Wait..`
-			);
+			const msg = await message.channel.send(`Wait..`);
 			msg.react("\uD83D\uDE0A");
 			msg.react("\uD83D\uDE21");
 			msg.react("\uD83D\uDE2D");
@@ -239,88 +259,54 @@ client.on("messageCreate", async (message) => {
 		crypto: async () => {
 			const msg = await message.channel.send("Loading...");
 
-			const result = await fetch(
-				`https://cryptingup.com/api/markets`
-			);
+			const result = await fetch(`https://cryptingup.com/api/markets`);
 			const json = await result.json();
 
-			json.markets = [...new Map(json.markets.map(item =>
-				[item["base_asset"], item])).values()];
+			json.markets = [
+				...new Map(
+					json.markets.map((item) => [item["base_asset"], item])
+				).values(),
+			];
 
 			json.markets.length = 15;
 
-			const embed = new EmbedBuilder()
-				.setTitle('Crypto prices');
-
+			const embed = new EmbedBuilder().setTitle("Crypto prices");
 
 			json.markets.map((coin) => {
 				embed.addFields([
-					{ name: `**${coin.base_asset}**`, value: ` *${parseFloat(coin.price).toFixed(8)}*$ ${parseInt(coin.change_24h) < 0 ? "🔻" : "🟢"} ${parseFloat(coin.change_24h).toFixed(2)}`, inline: true }
-				])
-			})
+					{
+						name: `**${coin.base_asset}**`,
+						value: ` *${parseFloat(coin.price).toFixed(8)}*$ ${
+							parseInt(coin.change_24h) < 0 ? "🔻" : "🟢"
+						} ${parseFloat(coin.change_24h).toFixed(2)}`,
+						inline: true,
+					},
+				]);
+			});
 
-			msg.edit({ embeds: [embed], content: "" })
-
+			msg.edit({ embeds: [embed], content: "" });
 		},
-		makeup: async () => {
-			const afgd = message.channel;
-
-			const canvas = createCanvas(300, 320)
-			const ctx = canvas.getContext('2d')
-
-			ctx.lineWidth = 10
-			ctx.strokeStyle = '#03a9f4'
-			ctx.fillStyle = '#03a9f4'
-
-			// Wall
-			ctx.strokeRect(75, 140, 150, 110)
-
-			// Door
-			ctx.fillRect(130, 190, 40, 60)
-
-			// Roof
-			ctx.beginPath()
-			ctx.moveTo(50, 140)
-			ctx.lineTo(150, 60)
-			ctx.lineTo(250, 140)
-			ctx.closePath()
-			ctx.stroke()
-
-			const { body } = await request(message.author.displayAvatarURL({ extension: 'jpg' }));
-			const avatar = await loadImage(await body.arrayBuffer());
-
-			// If you don't care about the performance of HTTP requests, you can instead load the avatar using
-			// const avatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ extension: 'jpg' }));
-
-			// Draw a shape onto the main canvas
-			ctx.drawImage(avatar, 25, 0, 200, canvas.height);
-
-			const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
-
-			afgd.send({ files: [attachment] });
-
-		}
 	};
 
 	if (commands[command]) {
 		commands[command]();
 	}
 
-	const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+	const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 	await rest.put(Routes.applicationCommands(client.user.id), {
 		body: [
 			{
-				name: 'ping',
-				description: 'yes'
-			}
-		]
+				name: "ping",
+				description: "yes",
+			},
+		],
 	});
 });
 
-client.on('interactionCreate', interaction => {
+client.on("interactionCreate", (interaction) => {
 	interaction.reply("Ok");
 	if (!interaction.isButton()) return;
-	interaction.message.delete()
+	interaction.message.delete();
 });
 
 client.login(process.env.TOKEN || "");
--- start.sh
+++
@@ -1,2 +0,0 @@
-export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
-node index
