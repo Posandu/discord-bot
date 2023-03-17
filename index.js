65821b9eeb6052993fe359cb1bec47258ccb4f64
Posandu Mapa <posandumapa@gmail.com>
2023-03-17 14:01:04 +0530

    j

--- index.js
+++ index.js
@@ -49,6 +49,37 @@ client.once("ready", async () => {
 	);
 
 	startMsg.react("🎉");
+
+	const generalChannel = client.channels.cache.get("990984025126608957");
+	/**
+	Send a random meme from reddit 
+	*/
+	const sendMeme = async () => {
+		const subreddits = ["dankmemes", "memes", "meirl", "me_irl", "funny"];
+		const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+
+		const data = await fetch(
+			`https://api.reddit.com/r/${subreddit}/random`
+		).then((res) => res.json());
+
+		const meme = data[0].data.children[0].data;
+
+		const embed = new EmbedBuilder()
+			.setTitle(meme.title)
+			.setURL(`https://reddit.com${meme.permalink}`)
+			.setImage(meme.url)
+			.setColor(0x00ff00);
+
+		debugChannel.send({
+			embeds: [embed],
+		});
+	};
+
+	// Send a meme every 10 minutes
+	setInterval(sendMeme, 1000 * 60 * 10);
+
+	// Send a meme on startup
+	sendMeme();
 });
 
 client.on("interactionCreate", async (interaction) => {
